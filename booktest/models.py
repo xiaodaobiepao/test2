7d65c6683f6116855396871ee53d98c8bc04a9d3
xiaodaobiepao <1310851368@qq.com>
2019-06-13 08:49:22 +0800

    models

--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date=models.DateTimeField()
+    bread=models.IntegerField(default=0)
+    bcommet=models.IntegerField(null=False)
+    isDelete=models.BooleanField(default=False)
+    class Meta:
+        db_table='bookinfo'
+
+class HeroInfo(models.Model):
+    hname=models.CharField(max_length=10)
+    hgender=models.BooleanField(default=True)
+    hcontent=models.CharField(max_length=1000)
+    isDelete=models.BooleanField(default=False)
+    book=models.ForeignKey(BookInfo)
