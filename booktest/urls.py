8fa87cf79161b7870cd9e08c1d6130642e8db3da
xiaodaobiepao <1310851368@qq.com>
2019-06-27 19:47:16 +0800

    show

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,5 +3,6 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^(\d+)$', views.detail)
+    url(r'^(\d+)$', views.detail),
+    url(r'^(?P<p2>\d+)/(?P<p3>\d+)/(?P<p1>\d+)/$', views.show)
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,6 @@ def index(request):
 
 def detail(request, r1):
     return  HttpResponse(r1)
+
+def show(request, p1, p2, p3):
+    return HttpResponse('year:%s,month:%s,day:%s'%(p1,p2,p3))
