aac473060de04a84036f3335b1713b4f7db49dc4
xiaodaobiepao <1310851368@qq.com>
2019-06-28 13:40:58 +0800

    get request

--- booktest/urls.py
+++ booktest/urls.py
@@ -4,5 +4,8 @@ import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^(\d+)$', views.detail, name='detail'),
-    url(r'^(?P<p2>\d+)/(?P<p3>\d+)/(?P<p1>\d+)/$', views.show)
+    url(r'^(?P<p2>\d+)/(?P<p3>\d+)/(?P<p1>\d+)/$', views.show),
+    url(r'^getTest1$', views.getTest1),
+    url(r'^getTest2$', views.getTest2),
+    url(r'^getTest3$', views.getTest3)
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -5,9 +5,24 @@ from django.http import HttpResponse
 def index(request):
     context={}
     return render(request,'booktest/index.html', context)
+    # return HttpResponse(request)
 
 def detail(request, r1):
     return  HttpResponse(r1)
 
 def show(request, p1, p2, p3):
     return HttpResponse('year:%s,month:%s,day:%s'%(p1,p2,p3))
+#zhanshilianjie
+def getTest1(request):
+    return render(request, 'booktest/getTest1.html')
+#接收一键一值
+def getTest2(request):
+    a=request.GET['a']
+    b=request.GET['b']
+    context={'a':a,'b':b}
+    return render(request, 'booktest/getTest2.html', context)
+#接收一键多值
+def getTest3(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    return render(request, 'booktest/getTest3.html')
